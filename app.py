f9bb902e2b0a0bf793e9c7c36c72399c9818aa61
tundrarich <57719391+tundrarich@users.noreply.github.com>
2021-03-29 16:11:39 +0100

    Fixing Repo

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-import sys, os
+import sys, os, git
 from flask import Flask
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 
 app = Flask(__name__)
 
+repo = git.Repo('.', search_parent_directories=True)
 
 FREEZER_BASE_URL = "http://localhost/tundrarich.github.io"
+FREEZER_DESTINATION = repo.working_tree_dir
+FREEZER_REMOVE_EXTRA_FILES = False
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -24,5 +27,8 @@ def page(path):
     return pages.get_or_404(path).html
 
 if __name__ == "__main__":
-    freezer.freeze()
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(port=8000)
 
