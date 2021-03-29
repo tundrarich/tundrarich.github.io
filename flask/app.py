5a1ba8607d55020c56766a42d62f0d318a0cba6c
tundrarich <57719391+tundrarich@users.noreply.github.com>
2021-03-29 15:42:21 +0100

    Testing Flask Deployement

---
+++ flask/app.py
@@ -0,0 +1,25 @@
+import sys, os
+from flask import Flask
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+
+pages = FlatPages(app)
+freezer = Freezer(app)
+
+@app.route("/")
+def index():
+    return "Testing Github"
+
+@app.route("/<path:path>/")
+def page(path):
+    return pages.get_or_404(path).html
+
+if __name__ == "__main__":
+    freezer.freeze()
+
---
+++ flask/pages/test.md
@@ -0,0 +1,4 @@
+title: Test
+
+
+Testing GitHub deployment
